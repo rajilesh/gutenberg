d59985e2e7777e6cd2ea96ac9802c539b8aa1cdb
Andrew Duthie <andrew@andrewduthie.com>
2017-10-31 12:13:28 -0400

    Revert "Components: Dismiss icon button tooltip when clicked"
    This reverts commit 0f938289ddd0d27c48da21940a7ec2f3e68fcee6.

--- components/icon-button/index.js
+++ components/icon-button/index.js
@@ -17,56 +17,21 @@ import Tooltip from '../tooltip';
 import Button from '../button';
 import Dashicon from '../dashicon';
 
+// This is intentionally a Component class, not a function component because it
+// is common to apply a ref to the button element (only supported in class)
 class IconButton extends Component {
-	constructor() {
-		super( ...arguments );
-
-		this.dismissTooltip = this.dismissTooltip.bind( this );
-		this.resetClicked = this.resetClicked.bind( this );
-
-		this.state = {
-			isClicked: false,
-		};
-	}
-
-	dismissTooltip() {
-		this.setState( {
-			isClicked: true,
-		} );
-
-		// Preserve original onClick prop behavior
-		const { onClick } = this.props;
-		if ( onClick ) {
-			onClick( ...arguments );
-		}
-	}
-
-	resetClicked() {
-		this.setState( {
-			isClicked: false,
-		} );
-	}
-
 	render() {
 		const { icon, children, label, className, tooltip, ...additionalProps } = this.props;
-		const { isClicked } = this.state;
 		const classes = classnames( 'components-icon-button', className );
 
 		let element = (
-			<Button
-				{ ...additionalProps }
-				aria-label={ label }
-				className={ classes }
-				onClick={ this.dismissTooltip }
-				onMouseLeave={ isClicked ? this.resetClicked : null }
-				onBlur={ isClicked ? this.resetClicked : null }
-			>
+			<Button { ...additionalProps } aria-label={ label } className={ classes }>
 				{ isString( icon ) ? <Dashicon icon={ icon } /> : icon }
 				{ children }
 			</Button>
 		);
 
-		if ( ! isClicked && label && ! children && false !== tooltip ) {
+		if ( label && ! children && false !== tooltip ) {
 			element = <Tooltip text={ tooltip || label }>{ element }</Tooltip>;
 		}
 
